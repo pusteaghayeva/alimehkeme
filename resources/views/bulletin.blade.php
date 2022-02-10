b46aa2dbf086d86dec511951325282860943bf10
Püstə <80460533+pusteaghayeva@users.noreply.github.com>
2022-02-10 20:19:05 +0400

    fixed bulletin pdf issue

--- resources/views/bulletin.blade.php
+++ resources/views/bulletin.blade.php
@@ -1,25 +1,32 @@
 @extends('main')
 @section('content')
-        <div class="about-content container mb-2">
-                <h3 class="about-title text-center">Bülletenlər</h3>
-                @forelse($bulletins as $bulletin)
-                <div class="">
-                    <p class="about-text"> {!! $bulletin->getTranslatedAttribute('content', $locale, 'fallbackLocale') !!}</p>
-                </div>
-            @empty
-                boshdur.
-            @endforelse
+    <div class="about-content container mb-2">
+        <h3 class="about-title text-center">Bülletenlər</h3>
+        @forelse($bulletins as $bulletin)
+            <div class="">
+                <p class="about-text"> {!! $bulletin->getTranslatedAttribute('content', $locale, 'fallbackLocale') !!}</p>
             </div>
+        @empty
+            boshdur.
+        @endforelse
+    </div>
 
-        <div class="first-down-menu container ">
-            <ul>
-                @forelse($bulletins as $bulletin)
-                    <li><a class="cool-link" href="{{asset('/storage/'.$bulletin->pdf)}}">{{$bulletin->getTranslatedAttribute('title', $locale, 'fallbackLocale')}}</a></li>
-                @empty
-                @endforelse
-            </ul>
-        </div>
+    <div class="first-down-menu container ">
+        <ul>
+            @forelse($bulletins as $bulletin)
+                @php $bulletin->pdf = json_decode($bulletin->pdf) @endphp
+            @empty($bulletin->pdf)
+                @continue
+            @else
+                <li><a class="cool-link"
+                       href="{{asset('/storage/'.$bulletin->pdf[0]->download_link)}}">{{$bulletin->getTranslatedAttribute('title', $locale, 'fallbackLocale')}}</a>
+                </li>
+            @endempty
+            @empty
+            @endforelse
+        </ul>
+    </div>
 
-        <div class="text-center"> {{$bulletins->appends(request()->input())->links()}}</div>
+    <div class="text-center"> {{$bulletins->appends(request()->input())->links()}}</div>
 
 @endsection
